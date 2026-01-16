018362727ade0266fb845547dbb18d6787c08181
sachnoikhoinghiep-cell <sachnoikhoinghiep@gmail.com>
2026-01-16 12:19:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/Veo-3-Action-Director---BeamNG-Style/',
       server: {
         port: 3000,
         host: '0.0.0.0',
